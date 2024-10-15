67c72af9a9915baafbeb56d1bf3a4a5cc78bafe9
SaiKiranMatta <saikirangoud.matta@gmail.com>
2024-10-16 02:37:12 +0530

    chore: update service to use mongodb

--- backend/internal/user/service.go
+++ backend/internal/user/service.go
@@ -1,54 +1,58 @@
 package user
 
 import (
+	"context"
 	"fmt"
-	"sync"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 type UserService struct {
-	users map[string]*User
-	mu    sync.RWMutex
+	collection *mongo.Collection
 }
 
-func NewUserService() *UserService {
+func NewUserService(db *mongo.Database) *UserService {
 	return &UserService{
-		users: make(map[string]*User),
+		collection: db.Collection("users"),
 	}
 }
 
 func (s *UserService) GetUser(id primitive.ObjectID) (*User, error) {
-	s.mu.RLock()
-	defer s.mu.RUnlock()
-	user, ok := s.users[id.Hex()]
-	if !ok {
-		return nil, fmt.Errorf("user not found")
+	var user User
+	err := s.collection.FindOne(context.Background(), bson.M{"_id": id}).Decode(&user)
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			return nil, fmt.Errorf("user not found")
+		}
+		return nil, err
 	}
-	return user, nil
+	return &user, nil
 }
 
 func (s *UserService) CreateUser(user *User) error {
-	s.mu.Lock()
-	defer s.mu.Unlock()
-	s.users[user.ID.Hex()] = user
-	return nil
+	_, err := s.collection.InsertOne(context.Background(), user)
+	return err
 }
 
 func (s *UserService) UpdateUser(user *User) error {
-	s.mu.Lock()
-	defer s.mu.Unlock()
-	s.users[user.ID.Hex()] = user
-	return nil
+	_, err := s.collection.UpdateOne(
+		context.Background(),
+		bson.M{"_id": user.ID},
+		bson.M{"$set": user},
+	)
+	return err
 }
 
 func (s *UserService) GetUserByEmail(email string) (*User, error) {
-	s.mu.RLock()
-	defer s.mu.RUnlock()
-	for _, user := range s.users {
-		if user.Email == email {
-			return user, nil
+	var user User
+	err := s.collection.FindOne(context.Background(), bson.M{"email": email}).Decode(&user)
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			return nil, fmt.Errorf("user not found")
 		}
+		return nil, err
 	}
-	return nil, fmt.Errorf("user not found")
+	return &user, nil
 }
