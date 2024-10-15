b001d731aa4d8198aa718c91fea57ce54e76601b
SaiKiranMatta <saikirangoud.matta@gmail.com>
2024-10-16 02:43:15 +0530

    chore: add finish registration proxy api

---
+++ frontend/src/app/api/auth/register/finish/route.ts
@@ -0,0 +1,23 @@
+import { NextResponse } from "next/server";
+
+export const POST = async (request: Request) => {
+    try {
+        const { userId, data } = await request.json();
+
+        const response = await fetch("http://localhost:8080/register/finish", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ userId, data }),
+        });
+
+        const responseBody = await response.text(); // Get the response body as text for logging
+
+        console.log("Response from Go server:", responseBody); // Log the response from Go server
+
+        return new NextResponse(responseBody, {
+            status: response.status,
+        });
+    } catch (err: any) {
+        return new NextResponse(`Error: ${err.message}`, { status: 500 });
+    }
+};
