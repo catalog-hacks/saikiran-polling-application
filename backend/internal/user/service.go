19c3cc315199576a84157f6c964fedfa745f09c5
SaiKiranMatta <saikirangoud.matta@gmail.com>
2024-10-15 11:53:44 +0530

    chore: add services for user model

--- backend/internal/user/service.go
+++ backend/internal/user/service.go
@@ -1,60 +1,54 @@
 package user
 
 import (
-	"context"
-	"errors"
-	"time"
+	"fmt"
+	"sync"
 
-	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
-	"go.mongodb.org/mongo-driver/mongo"
 )
 
-type UserService struct{
-	db *mongo.Collection
+type UserService struct {
+	users map[string]*User
+	mu    sync.RWMutex
 }
 
-func NewUserService(db *mongo.Collection) *UserService {
-    return &UserService{db: db}
+func NewUserService() *UserService {
+	return &UserService{
+		users: make(map[string]*User),
+	}
 }
 
-func (s *UserService) RegisterUser(user *User) error {
-    user.CreatedAt = time.Now().Unix()
-    _, err := s.db.InsertOne(context.TODO(), user)
-    return err
+func (s *UserService) GetUser(id primitive.ObjectID) (*User, error) {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+	user, ok := s.users[id.Hex()]
+	if !ok {
+		return nil, fmt.Errorf("user not found")
+	}
+	return user, nil
 }
 
-func (s *UserService) FindUserByEmail(email string) (*User, error) {
-    var user User
-    err := s.db.FindOne(context.TODO(), bson.M{"email": email}).Decode(&user)
-    if err != nil {
-        return nil, err
-    }
-    return &user, nil
+func (s *UserService) CreateUser(user *User) error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	s.users[user.ID.Hex()] = user
+	return nil
 }
 
-func (s *UserService) AddWebAuthnCredential(userID primitive.ObjectID, cred WebAuthnCredential) error {
-    _, err := s.db.UpdateOne(
-        context.TODO(),
-        bson.M{"_id": userID},
-        bson.M{"$push": bson.M{"webauthn_creds": cred}},
-    )
-    return err
+func (s *UserService) UpdateUser(user *User) error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	s.users[user.ID.Hex()] = user
+	return nil
 }
 
-
-// VerifyCredential checks if the provided credential matches the stored WebAuthn credential.
-func (s *UserService) VerifyCredential(userID primitive.ObjectID, credentialID string) (*WebAuthnCredential, error) {
-    var user User
-    err := s.db.FindOne(context.TODO(), bson.M{"_id": userID}).Decode(&user)
-    if err != nil {
-        return nil, err
-    }
-
-    for _, cred := range user.WebAuthnCreds {
-        if cred.CredentialID == credentialID {
-            return &cred, nil
-        }
-    }
-    return nil, errors.New("credential not found")
+func (s *UserService) GetUserByEmail(email string) (*User, error) {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+	for _, user := range s.users {
+		if user.Email == email {
+			return user, nil
+		}
+	}
+	return nil, fmt.Errorf("user not found")
 }
