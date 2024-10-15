2c5262a508e5fb8b60f2afb740df6177d3f6032f
SaiKiranMatta <saikirangoud.matta@gmail.com>
2024-10-15 11:53:00 +0530

    chore: update user model for webauthn

--- backend/internal/user/model.go
+++ backend/internal/user/model.go
@@ -1,22 +1,47 @@
+// internal/user/model.go
 package user
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
+import (
+	"github.com/go-webauthn/webauthn/webauthn"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 type User struct {
-	Id primitive.ObjectID `bson:"_id,omitempty" json:"id"`
-	Username string `bson:"username" json:"username"`
-	Email string `bson:"email" json:"email"`
-	CreatedAt int64 `bson:"created_at" json:"created_at"`
-	CreatedPolls []primitive.ObjectID `bson:"created_polls" json:"created_polls"`
-	WebAuthnCreds []WebAuthnCredential `bson:"webauthn_creds" json:"webauthn_creds"`
+	ID             primitive.ObjectID   `bson:"_id" json:"id"`
+	Name           string               `bson:"name" json:"name"`
+	DisplayName    string               `bson:"display_name" json:"display_name"`
+	Email          string               `bson:"email" json:"email"`
+	CreatedPolls   []primitive.ObjectID `bson:"created_polls" json:"created_polls"`
+	Credentials    []webauthn.Credential `bson:"credentials" json:"credentials"`
 }
 
+// WebAuthnUser is an adapter that implements the webauthn.User interface
+type WebAuthnUser struct {
+	*User
+}
 
-type WebAuthnCredential struct {
-	ID string `bson:"_id" json:"id"`
-	PublicKey []byte `bson:"public_key" json:"public_key"`
-	CredentialID string `bson:"credential_id" json:"credential_id"`
-	SignCount uint32 `bson:"sign_count" json:"sign_count"`
-	AuthenticatorData []byte `bson:"authenticator_data" json:"authenticator_data"`
+func (u *WebAuthnUser) WebAuthnID() []byte {
+	return u.ID[:]
+}
+
+func (u *WebAuthnUser) WebAuthnName() string {
+	return u.Name
+}
+
+func (u *WebAuthnUser) WebAuthnDisplayName() string {
+	return u.DisplayName
+}
+
+func (u *WebAuthnUser) WebAuthnIcon() string {
+	return ""
+}
+
+func (u *WebAuthnUser) WebAuthnCredentials() []webauthn.Credential {
+	return u.Credentials
+}
+
+// NewWebAuthnUser creates a new WebAuthnUser from a User
+func NewWebAuthnUser(u *User) *WebAuthnUser {
+	return &WebAuthnUser{User: u}
 }
 
