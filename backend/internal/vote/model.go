9f9f97680649d24fdc3a8c044072cd5b841b4d30
SaiKiranMatta <saikirangoud.matta@gmail.com>
2024-10-17 15:19:32 +0530

    chore: update vote model

--- backend/internal/vote/model.go
+++ backend/internal/vote/model.go
@@ -7,6 +7,7 @@ import (
 )
 
 type Vote struct {
+	PollID    primitive.ObjectID   `bson:"poll_id" json:"poll_id"`
 	UserID    primitive.ObjectID   `bson:"user_id" json:"user_id"`
 	OptionIDs []primitive.ObjectID `bson:"option_ids" json:"option_ids"`
 	VotedAt   time.Time            `bson:"voted_at" json:"voted_at"`
