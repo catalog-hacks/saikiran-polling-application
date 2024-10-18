4b2b2d79269d9f8ec58f3abc834ce085d193c627
SaiKiranMatta <saikirangoud.matta@gmail.com>
2024-10-18 11:19:59 +0530

    chore: switch to using protected route component

--- frontend/src/app/polls/new/page.tsx
+++ frontend/src/app/polls/new/page.tsx
@@ -2,38 +2,23 @@
 
 import CreatePollForm from "@/components/CreatePoll";
 import { NextPage } from "next";
-import { useSession } from "next-auth/react";
-import { redirect } from "next/navigation";
-import { useEffect, useState } from "react";
+import ProtectedRoute from "@/components/ProtectedRoute";
+import { useSessionStore } from "@/store/useSessionStore";
+import { useShallow } from "zustand/shallow";
 
 const CreatePollPage: NextPage = () => {
-    const { data: session, status } = useSession();
-    const [loading, setLoading] = useState(true);
-
-    useEffect(() => {
-        if (status === "authenticated") {
-            setLoading(false);
-        } else if (status === "loading") {
-            // Session is still loading, do nothing
-        } else {
-            redirect("/");
-        }
-    }, [status]);
-
-    if (loading) {
-        return <div>Loading...</div>;
-    }
-
-    // If session is available, pass the user ID to PollForm
-    const user_id = session?.user?.id;
-    const email = session?.user?.email;
+    const [user_id, email] = useSessionStore(
+        useShallow((state) => [state.user_id, state.email])
+    );
 
     return (
-        <div className=" pt-8">
-            {user_id && email && (
-                <CreatePollForm user_id={user_id} email={email} />
-            )}
-        </div>
+        <ProtectedRoute>
+            <div className="pt-8">
+                {user_id && email && (
+                    <CreatePollForm user_id={user_id} email={email} />
+                )}
+            </div>
+        </ProtectedRoute>
     );
 };
 
