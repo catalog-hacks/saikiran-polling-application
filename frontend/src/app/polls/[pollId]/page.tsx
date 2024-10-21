b0adaf207f66cfc70cb824325f79bfb36fb944aa
SaiKiranMatta <saikirangoud.matta@gmail.com>
2024-10-21 16:20:25 +0530

    fix: setting initial poll data

--- frontend/src/app/polls/[pollId]/page.tsx
+++ frontend/src/app/polls/[pollId]/page.tsx
@@ -22,7 +22,7 @@ async function getPollData(pollId: string): Promise<PollWithUserVote> {
     }
 
     const data = await res.json();
-    return data.poll;
+    return data;
 }
 
 export default async function PollPage({
