91fc2eaa15b05a556405dca9b4e65364e62c6d60
SaiKiranMatta <saikirangoud.matta@gmail.com>
2024-10-22 11:36:21 +0530

    chore: add description to poll creation api

--- frontend/src/app/api/polls/route.ts
+++ frontend/src/app/api/polls/route.ts
@@ -1,13 +1,19 @@
 import { NextResponse } from "next/server";
 
 export const POST = async (request: Request) => {
-    const { question, options, user_id, multiple_choices } =
+    const { question, description, options, user_id, multiple_choices } =
         await request.json();
     const backendUrl = process.env.BACKEND_URL;
     const response = await fetch(`${backendUrl}/polls`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ question, options, user_id, multiple_choices }),
+        body: JSON.stringify({
+            question,
+            description,
+            options,
+            user_id,
+            multiple_choices,
+        }),
     });
 
     const data = await response.json();
